dcfb9cb165e9499697dc5364e91283d3cd1b9a29
Abdulkadir <qansax30@hotmail.com>
2024-11-10 22:54:10 +0100

    Add project files.

---
+++ Function/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    
+    
+        
+
+
+        
+            namespace functions
+    {
+        class test
+
+        {
+            public static void something()
+            {
+                Console.WriteLine();
+            }
+            class program
+            {
+                static void Main(string[] args)
+                {
+
+                    WelcomeMessage();
+
+                    Console.ReadLine();
+                }
+
+                static void WelcomeMessage()
+                {
+                    Console.WriteLine("Welcome Abdulkadir");
+                }
+            }
+        }
+    }
